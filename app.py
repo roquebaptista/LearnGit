b1982cb3f4c8bd2942281bc9a7fd6e12032a6c2b
Roque Baptista <roquebaptista@hotmail.com>
2023-05-27 20:20:22 +0200

    Added an extra user and updated requirements

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Bottle(__name__)
 
 @app.route("/")
 def index():
-    return '{ "id": 1, "user": "u7823" }'
+    return '[ { "id": 1, "user": "U1232" }, { "id": 2, "user": "U1245" } ]'
 
 if __name__ == "__main__":
     app.run(debug=True, reloader=True)
